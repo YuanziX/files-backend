0de045479ae572233b41a2d7314a7de334cf6b13
YuanziX <achubadyal4@gmail.com>
2025-09-18 13:56:09 +0530

    utils/auth: cleanup

--- internal/utils/auth.utils.go
+++ internal/utils/auth.utils.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/YuanziX/files-backend/internal/middleware"
 )
@@ -20,18 +19,4 @@ func GetUserID(ctx context.Context) (string, bool) {
 func GetRole(ctx context.Context) (string, bool) {
 	role, ok := ctx.Value(middleware.RoleKey).(string)
 	return role, ok
-}
-
-func RequiresRole(ctx context.Context, role string) error {
-	userRole, ok := GetRole(ctx)
-	if !ok {
-		return fmt.Errorf("unauthorized: no role found")
-	}
-	if role == USER_ROLE && userRole == USER_ROLE {
-		return nil
-	} else if role == ADMIN_ROLE && userRole == ADMIN_ROLE {
-		return nil
-	}
-
-	return fmt.Errorf("forbidden: insufficient permissions")
 }
